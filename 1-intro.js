d808904e087ef9553cc0143d33d2b705e9294be3
zoh01 <adebayozoh@gmail.com>
2025-03-09 15:17:10 +0100

    First Commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const population = 25
+
+if (population < 20) {
+  console.log(`Small Population`);
+} else {
+  console.log(`Large Population`);
+}
+
+console.log(`Hello, This is my first Node App`);
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = "SUPER SECRET"
+// share
+const zoh = "Zoh"
+const wariz = "Wariz"
+
+module.exports = { zoh, wariz }
